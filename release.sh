36b25c983eab04dc18ce20759c152b20f76eebff
Andres Salomon <dilinger@debian.org>
2008-10-28 00:27:09 -0400

    release: don't bail if ext3/jffs2 directories already exist
    Signed-off-by: Andres Salomon <dilinger@debian.org>

--- release.sh
+++ release.sh
@@ -3,7 +3,7 @@
 # prepare a release
 for f in *.packages; do
 	desktop=$(echo $f | sed 's/\.packages//')
-	mkdir ext3 jffs2
+	mkdir -p ext3 jffs2
 	./initchroot.sh --package-list ${f} debxo-${desktop}
 	./mkjffs2.sh debxo-${desktop} jffs2/${desktop}.img
 	./mkext3.sh debxo-${desktop} ext3/debxo-${desktop}.ext3.img
