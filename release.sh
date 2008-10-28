6a8e18876f3564096f223dd2e45c6b5fad17e242
Andres Salomon <dilinger@debian.org>
2008-10-28 00:19:55 -0400

    release: FAIL
    Signed-off-by: Andres Salomon <dilinger@debian.org>

--- release.sh
+++ release.sh
@@ -3,7 +3,7 @@
 # prepare a release
 for f in *.packages; do
 	desktop=$(echo $f | sed 's/\.packages//')
-	./initchroot.sh debxo-${desktop}
+	./initchroot.sh --package-list ${f} debxo-${desktop}
 	./mkjffs2.sh debxo-${desktop} ${desktop}.img
 	./mkext3.sh debxo-${desktop} debxo-${desktop}.ext3.img
 	gzip debxo-${desktop}.ext3.img
