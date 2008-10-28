61b6253f6c675358ba9eb475c103ac73f2267783
Andres Salomon <dilinger@debian.org>
2008-10-28 13:29:35 -0400

    release: don't use subdirectories until after images are created
    gnome.img references jffs2/gnome.dat; that's incorrect.
    
    Signed-off-by: Andres Salomon <dilinger@debian.org>

--- release.sh
+++ release.sh
@@ -3,12 +3,15 @@
 # prepare a release
 for f in *.packages; do
 	desktop=$(echo $f | sed 's/\.packages//')
-	mkdir -p ext3 jffs2
 	./initchroot.sh --package-list ${f} \
 			--local-apt-mirror "deb http://localhost:9999/debian lenny main contrib non-free
 deb http://localhost:9999/security lenny/updates main contrib non-free" \
 			debxo-${desktop}
-	./mkjffs2.sh debxo-${desktop} jffs2/${desktop}.img
-	./mkext3.sh debxo-${desktop} ext3/debxo-${desktop}.ext3.img
-	gzip ext3/debxo-${desktop}.ext3.img
+	./mkjffs2.sh debxo-${desktop} ${desktop}.img
+	./mkext3.sh debxo-${desktop} debxo-${desktop}.ext3.img
+	gzip debxo-${desktop}.ext3.img
+
+	mkdir -p ext3 jffs2
+	mv debxo-${desktop}.ext3.img.gz ext3
+	mv ${desktop}.img ${desktop}.dat jffs2
 done
