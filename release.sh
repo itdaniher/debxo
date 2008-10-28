5f460655b64747b2794dd4af190add96d88a80f9
Andres Salomon <dilinger@debian.org>
2008-10-28 00:25:55 -0400

    release: stick images into subdirectories Signed-off-by: Andres Salomon <dilinger@debian.org>

--- release.sh
+++ release.sh
@@ -3,8 +3,9 @@
 # prepare a release
 for f in *.packages; do
 	desktop=$(echo $f | sed 's/\.packages//')
+	mkdir ext3 jffs2
 	./initchroot.sh --package-list ${f} debxo-${desktop}
-	./mkjffs2.sh debxo-${desktop} ${desktop}.img
-	./mkext3.sh debxo-${desktop} debxo-${desktop}.ext3.img
-	gzip debxo-${desktop}.ext3.img
+	./mkjffs2.sh debxo-${desktop} jffs2/${desktop}.img
+	./mkext3.sh debxo-${desktop} ext3/debxo-${desktop}.ext3.img
+	gzip ext3/debxo-${desktop}.ext3.img
 done
