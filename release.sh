fe97d0d0f72bf35c995439610ed163e1742bdc3e
Andres Salomon <dilinger@debian.org>
2008-10-28 01:04:14 -0400

    release: use new --local-apt-mirror + approx
    Signed-off-by: Andres Salomon <dilinger@debian.org>

--- release.sh
+++ release.sh
@@ -4,7 +4,10 @@
 for f in *.packages; do
 	desktop=$(echo $f | sed 's/\.packages//')
 	mkdir -p ext3 jffs2
-	./initchroot.sh --package-list ${f} debxo-${desktop}
+	./initchroot.sh --package-list ${f} \
+			--local-apt-mirror "deb http://localhost:9999/debian lenny main contrib non-free
+deb http://localhost:9999/security lenny/updates main contrib non-free" \
+			debxo-${desktop}
 	./mkjffs2.sh debxo-${desktop} jffs2/${desktop}.img
 	./mkext3.sh debxo-${desktop} ext3/debxo-${desktop}.ext3.img
 	gzip ext3/debxo-${desktop}.ext3.img
