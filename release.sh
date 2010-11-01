5bf196477987f292d3b83c9997e11e24059dcd4b
Andres Salomon <dilinger@queued.net>
2010-11-01 01:52:19 -0700

    debxo: update release script to use squeeze and ubifs
    Signed-off-by: Andres Salomon <dilinger@queued.net>

--- release.sh
+++ release.sh
@@ -4,16 +4,16 @@
 for f in configs/debxo-*; do
 	desktop=$(basename $f | sed 's/debxo-//')
 	./mkchroot.sh --config-type debxo-${desktop} \
-			--local-apt-mirror "deb http://localhost:9999/debian lenny main contrib non-free
-deb http://localhost:9999/security lenny/updates main contrib non-free
-deb http://localhost:9999/backports lenny-backports main contrib non-free" \
+			--local-apt-mirror "deb http://localhost:9999/debian squeeze main contrib non-free
+deb http://localhost:9999/security squeeze/updates main contrib non-free
+deb http://localhost:9999/backports squeeze-backports main contrib non-free" \
 			debxo-${desktop}
-	./mkjffs2.sh --config-type debxo-${desktop} debxo-${desktop} ${desktop}.img
+	./mkubifs.sh --config-type debxo-${desktop} debxo-${desktop} ${desktop}.img
 	./mkext3.sh --config-type debxo-${desktop} debxo-${desktop} \
 			debxo-${desktop}.ext3.img
 	gzip debxo-${desktop}.ext3.img
 
-	mkdir -p ext3 jffs2
+	mkdir -p ext3 nand
 	mv debxo-${desktop}.ext3.img.gz ext3
-	mv ${desktop}.img ${desktop}.dat jffs2
+	mv ${desktop}.img ${desktop}.dat nand
 done
